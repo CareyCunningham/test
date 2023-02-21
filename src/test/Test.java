c4a1dda10274fd50285c81e4e0af33152095f22c
CareyCunningham <cunningc@sheridancollege.com>
2023-02-20 21:13:29 -0500

    test

---
+++ src/test/Test.java
@@ -0,0 +1,15 @@
+package test;
+
+/**
+ * SYST17796 - Fundamentals of Software Design and Development
+ * @author Carey Cunningham
+ */
+public class Test {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    }
+
+}
